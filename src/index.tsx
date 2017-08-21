1ba2e9ea03f07bcdf13a45d354cea4896114faa6
Batman@ZiiOS <aobphilos@gmail.com>
2017-08-21 18:43:00 +0700

    add draft version

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const TestApp = () => {
+  return (
+    <div>
+      <p>Hello Boo!</p>
+    </div>
+  )
+}
+
+ReactDOM.render(<TestApp />, document.getElementById('app'))
